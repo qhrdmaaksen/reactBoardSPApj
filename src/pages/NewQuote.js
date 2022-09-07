01cb347f5d2d47d67e94add0fa48b2d5e966359c
vitamin777 <rlaalsdn8@gmail.com>
2022-09-07 19:07:43 +0900

    feat : 새 게시글 등록 후 게시글 목록으로 이동되도록 함
    -NewQuote.js 에서 react-router-dom 에있는 useHistory 를 활용해 push stack 으로 게시글 목록 경로인 /quotes 지정하여 사용자가 뒤로가기도 가능하도록하고 목록으로 바로 이동되도록 함
    
    -적용 및 정상 출력 확인

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,15 @@
-import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from 'react-router-dom';
+import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
-	const addQuoteHandler = (quoteData) => {
-		console.log(quoteData)
-	}
-	return (
-			<QuoteForm onAddQuote={addQuoteHandler} />
-	)
-}
+  const history = useHistory();
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+		/*페이지를 불러올수있는 useHistory 활용해 stack 에있는 push 로 /quotes 경로 지정
+		* -quotes 로 이동하도록 하였으며 push 를 사용했기때문에 사용자는 뒤로가기가 가능함
+		* -뒤로가기 불가를 하고 싶다면 push 가 아닌 replace stack 을 사용하면됨*/
+    history.push('/quotes');
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
+};
 export default NewQuote;
