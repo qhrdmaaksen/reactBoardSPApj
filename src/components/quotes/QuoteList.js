e84e1afe627cc1ed55d393d067c06a442f896daf
vitamin777 <rlaalsdn8@gmail.com>
2022-09-09 21:39:43 +0900

    feat : 게시물 목록 오름차순 내림차순 버튼 기능추가
    -QuoteList.js 에서 button onClick Fn changeSortingHandler 를 만들어 history stack push 에 쿼리 매개변수 경로 설정 후 URLSearch params 를 가져오려고 useLocation 을 활용해 isSortingAscending state 를 만들어서 해당 state 에따라서 asc desc 로 바뀌게함
    
    -적용 및 정상 출력 확인

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,23 +1,59 @@
-import { Fragment } from 'react';
-
+import {Fragment} from 'react';
+import {useHistory, useLocation} from 'react-router-dom';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
-const QuoteList = (props) => {
-  return (
-    <Fragment>
-      <ul className={classes.list}>
-        {props.quotes.map((quote) => (
-          <QuoteItem
-            key={quote.id}
-            id={quote.id}
-            author={quote.author}
-            text={quote.text}
-          />
-        ))}
-      </ul>
-    </Fragment>
-  );
+/*이 함수는 인용문 배열을 id로 정렬해 주며 오름차순, 내림차순으로 정렬해서,
+그다음 오름차순, 내림차순으로 정렬해서, 더 크거나 작은 id를 상위에 띄워줌
+sortQuotes 를 통해 이 컴포넌트 함수에서 정렬된 인용문을 띄울 수 있게됨
+sortQuotes 가 렌더링 될 때마다 정렬된 인용문이 뜸*/
+const sortQuotes = (quotes, ascending) => {
+	return quotes.sort((quoteA, quoteB) => {
+		if (ascending) {
+			return quoteA.id > quoteB.id ? 1 : -1;
+		} else {
+			return quoteA.id < quoteB.id ? 1 : -1;
+		}
+	});
+};
+
+const QuoteList = props => {
+	const history = useHistory();
+	const location = useLocation();
+	console.log(location);
+
+	/*쿼리 매개 변수 key, value 를 가져올 객체 queryParams*/
+	const queryParams = new URLSearchParams(location.search);
+
+	/*location 의 search 에서 추출한 key sort 의 value 가 asc 인지 desc 인지*/
+	const isSortingAscending = queryParams.get('sort') === 'asc';
+
+	const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+	/*쿼리 매개 변수가 true(asc) false(desc) 인지에따라 변환*/
+	const changeSortingHandler = () => {
+		history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+	};
+
+	return (
+			<Fragment>
+				<div className={classes.sorting}>
+					<button onClick={changeSortingHandler}>
+						Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+					</button>
+				</div>
+				<ul className={classes.list}>
+					{sortedQuotes.map(quote => (
+							<QuoteItem
+									key={quote.id}
+									id={quote.id}
+									author={quote.author}
+									text={quote.text}
+							/>
+					))}
+				</ul>
+			</Fragment>
+	);
 };
 
 export default QuoteList;
