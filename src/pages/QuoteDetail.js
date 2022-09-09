a4aff5831e4d575f3e9095b3d8256ec0ce6b90d1
vitamin777 <rlaalsdn8@gmail.com>
2022-09-09 23:55:02 +0900

    feat : quote detail 에 comments 보기 추가
    -QuoteDetail.js 에서 Route 로 게시글 상세보기로 왔을때 exact 속성으로 일치하는 경로 /comments 로 이동시켜 comments component 렌더링함
    
    -적용 및 정상 출력 확인

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,43 +1,49 @@
-import {Fragment} from 'react';
-import {Route, useParams} from 'react-router-dom';
-import Comments from "../components/comments/Comments";
-import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import { Fragment } from 'react';
+import { Link, Route, useParams } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
 const QuoteDetail = () => {
   const params = useParams();
-	const DUMMY_DATA = [
-		{
-			id: 'u1',
-			author: 'min',
-			text: 'min is very nice',
-		},
-		{
-			id: 'u2',
-			author: 'vitamin',
-			text: 'vitamin is very nice',
-		},
-		{
-			id: 'u3',
-			author: 'vitamin777',
-			text: 'vitamin777 is very nice',
-		},
-	];
-	/*params 는 /quotes 가 들어가며 그 이후	quoteId 는 App.js 에서 설정해놓은 url 의 quoteId 임
-	* --url 에 들어온 id 와 게시글 목록의 id 가 같다면 quote 에 대입*/
-	const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId)
-	/*url 에 직접 존재하지않는 quote.id 를 입력하여 검색 시*/
-	if (!quote){
-		return (
-				<p>게시글을 찾을 수 없습니다.</p>
-		)
-	}
+  const DUMMY_DATA = [
+    {
+      id: 'u1',
+      author: 'min',
+      text: 'min is very nice',
+    },
+    {
+      id: 'u2',
+      author: 'vitamin',
+      text: 'vitamin is very nice',
+    },
+    {
+      id: 'u3',
+      author: 'vitamin777',
+      text: 'vitamin777 is very nice',
+    },
+  ];
+  /*params 는 /quotes 가 들어가며 그 이후	quoteId 는 App.js 에서 설정해놓은 url 의 quoteId 임
+   * --url 에 들어온 id 와 게시글 목록의 id 가 같다면 quote 에 대입*/
+  const quote = DUMMY_DATA.find(quote => quote.id === params.quoteId);
+  /*url 에 직접 존재하지않는 quote.id 를 입력하여 검색 시*/
+  if (!quote) {
+    return <p>게시글을 찾을 수 없습니다.</p>;
+  }
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
-			{/*중첩 라우트로 동적 라우팅 설정*/}
-			<Route path={`/quotes/${params.quoteId}/comments`}>
-				<Comments />
-			</Route>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+			{/*URL 을 근거로 다른 콘텐츠를 상황에 따라 렌더함*/}
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            댓글 불러오기
+          </Link>
+        </div>
+      </Route>
+      {/*중첩 라우트로 동적 라우팅 설정*/}
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
