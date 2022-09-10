914099dae28b59aa8714258a0be83ef8805f5231
vitamin777 <rlaalsdn8@gmail.com>
2022-09-11 01:13:39 +0900

    feat : 댓글 목록 및 댓글 추가 기능 추가
    -Comment.js 에서 useHttp(getAllComments) 활용하여 sendRequest 로 댓글 목록 불러오기 요청을 quoteId 를 활용하여 구현,status 에따라 상태 관리하였음
    -Comment.js 에서 댓글 데이터는 loadedComments 로 getAllComments 에서 추출하여 보여줬음
    -NewCommentForm 에서 새 댓글은 useHttp(addComment)를 활용해서 sendRequest 를 추출해 댓글 추가 요청하였으며 status 에따라 상태 관리하였음
    -NewCommentForm 에서 text 는 useRef 활용하여 commentTextRef.current.value 로 받아 enteredText 에 대입하여 commentData 의 text:enteredText 로 입력받음
    
    -적용 및 정상 출력 확인

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,25 +1,58 @@
-import { useState } from 'react';
-
+import {useCallback, useEffect, useState} from 'react';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import { useParams } from 'react-router-dom';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+  const { quoteId } = params;
+  const { sendRequest, status, data: loadedComments, error } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  /*결국 상위 컴포넌트가 다시 렌더링될 때마다 다시 생성될 것을 방지하기위해 useCallback 사용
+  * 컴포넌트가 재평가될 때마다 이 함수가 다시 생성되지 않도록 하는 것,그래서 불필요한 재렌더링 주기와
+  * 무한 루프를 방지하기 위해 useCallback 사용*/
+  const addedCommentHandler = useCallback(()=> {
+    sendRequest(quoteId)
+  },[sendRequest,quoteId]);
+
+  let comments;
+  if (status === 'pending') {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (status === 'completed' && !error && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments = <p className="centered">아직 추가된 댓글이 없습니다.</p>;
+  }
+
   return (
     <section className={classes.comments}>
       <h2>사용자 댓글</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           댓글 추가
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>댓글...</p>
+      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={addedCommentHandler} />}
+      {comments}
     </section>
   );
 };
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,26 +1,46 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
 
 import classes from './NewCommentForm.module.css';
+import LoadingSpinner from '../UI/LoadingSpinner';
 
-const NewCommentForm = (props) => {
+const NewCommentForm = props => {
   const commentTextRef = useRef();
+  const { sendRequest, status, error } = useHttp(addComment);
+  const { onAddedComment } = props;
+  useEffect(() => {
+    /*에러가 없고 요청 완료시 onAddedComment Fn 호출*/
+    if (status === 'completed' && !error) {
+      onAddedComment();
+    }
+  }, [status,error,onAddedComment]);
 
-  const submitFormHandler = (event) => {
+  const submitFormHandler = event => {
     event.preventDefault();
-
-    // optional: Could validate here
-
-    // send comment to server
+    const enteredText = commentTextRef.current.value;
+    sendRequest({
+      commentData: {
+        text: enteredText,
+        quoteId: props.quoteId,
+      },
+    });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {/*리퀘스트 진행 중일때 로딩 스피너*/}
+      {status === 'pending' && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>당신의 댓글</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">당신의 댓글</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>댓글 추가</button>
+        <button className="btn">댓글 추가</button>
       </div>
     </form>
   );
